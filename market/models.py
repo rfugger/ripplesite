9fb243a4f85670ae8b57750ee7bbf40ef873793b
Thomas Hartman <thomashartman1@gmail.com>
2008-04-26 22:04:21 -0400

    search form tweaks

--- market/models.py
+++ market/models.py
@@ -6,11 +6,13 @@ class Advertisement(models.Model):
     user = models.ForeignKey(Node)
     title = models.CharField(max_length=255)
     text = models.TextField()
+    location = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
     posted_date = models.DateTimeField(auto_now_add=True)
 
     class Admin:
-        list_display = ('user', 'title', 'posted_date')
-        search_fields = ('user', 'title', 'text')
+        list_display = ('user', 'title', 'location', 'category', 'posted_date')
+        search_fields = ('user', 'title', 'location', 'category', 'text')
 
     def __str__(self):
         return self.title
